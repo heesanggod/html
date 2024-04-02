321379b8dcde0b1e12e7eef587cd05684d893137
parksungeon <un109101@naver.com>
2024-04-02 09:51:05 +0900

    2024.04.02 9:50

---
+++ Ts/2.type/type.ts
@@ -0,0 +1,56 @@
+/*
+    타입선언 : 
+    - 변수 및 함수에 타입을 지정하여 타입에 대한 제한을 추가할 수 있음
+*/
+let variable: string = '문자열';
+// variable = true; 'boolean' 형식은 'string' 형식에 할당할 수 없습니다.
+
+function func(arg : number): string {
+    return arg + '';   // 'number' 형식은 'string' 형식에 할당할 수 없습니다.  + ''; 로 String 형식을 할당해준다
+}
+
+const arrowFunc = (arg: number): string => {
+    return arg +'';
+}
+
+/*
+    typescript의 타입
+    - 
+    -
+    - 
+    - 
+    - 
+    - 
+    - 
+    - 
+    - 
+    - 
+*/
+
+
+/*
+    타입추론 :
+    - 
+*/
+
+
+
+/*
+    타입 변환 :
+    - 
+*/
+
+
+
+/*
+    유니온 타입
+    - 
+*/
+
+
+
+/*
+    리터럴 타입
+    - 
+*/
+
